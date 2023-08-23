73b3b8d58963c39d68d7e5bc29a1d247c580b2a5
* <bodex896.alabiolabode6@gmail.com>
2023-08-23 03:34:34 +0100

    my  commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author clement bodex <https://github.com/bodex896>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
